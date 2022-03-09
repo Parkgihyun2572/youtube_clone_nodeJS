e5782cad0fe0d8a9b584f7f01e7a3c494402156b
Gihyun Park <einght4046@naver.com>
2022-03-10 05:53:28 +0900

    about what is the Middelware & how make global middleware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 import express, { request, response } from "express";
+import res from "express/lib/response";
 
 const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (request, response, next) => {
-    console.log("I'm in the middle!");
+const logger = (request, response, next) => {
+    console.log(`${request.method} ${request.url}`);
+    next();
+}
+
+const privateMiddleware = (request, response, next) => {
+    const url = request.url;
+    if (url === "/protected") {
+        return response.send("<h1>Not Allowed</h1>")
+    }
+    console.log("Allowed, you may continue.");
     next();
 }
 
@@ -13,7 +23,14 @@ const handleHome = (request, response, next) => {
     return response.end();
 }
 
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (request, response) => {
+    return response.send("Welcome to the private lounge!");
+}
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handelListening = () => {
     console.log(`✔ Server Listening on port  http://localhost:${PORT}`);
