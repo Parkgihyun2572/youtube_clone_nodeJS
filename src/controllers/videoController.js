1e5f88b300ffd2e60d1572be77fe2c9efc06f287
Gihyun Park <einght4046@naver.com>
2022-07-08 14:38:55 +0900

    create pug files

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,7 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search Video");
 
 export const uploadVideo = (req, res) => res.send("Upload Video");
-export const seeVideo = (req, res) => {
-    console.log(req.params);
-    res.send("Watch Video");
-}
+export const seeVideo = (req, res) => res.render("watch");
 export const editVideo = (req, res) => res.send("Edit Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
