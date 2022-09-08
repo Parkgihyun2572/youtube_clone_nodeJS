2baf76f3a0ba8ffed7803f671a7f59fa6825cc1f
ParkGiHyun <einght4046@naver.com>
2022-09-08 14:22:23 +0900

    add basic pug(partials, extends, variables)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
 export const search = (req, res) => res.send("Search Video");
 
 export const uploadVideo = (req, res) => res.send("Upload Video");
-export const seeVideo = (req, res) => res.render("watch");
-export const editVideo = (req, res) => res.send("Edit Video");
+export const seeVideo = (req, res) => res.render("watch", {pageTitle: "Watch Video"});
+export const editVideo = (req, res) => res.render("edit", {pageTitle: "Edit Video"});
 export const deleteVideo = (req, res) => res.send("Delete Video");
