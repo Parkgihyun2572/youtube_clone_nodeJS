c89086b38f5b57599af159c1e98ebb523b08b7d4
ParkGiHyun <einght4046@naver.com>
2022-10-03 08:23:08 +0900

    change liite bit..

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,9 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home"});
+import Video from "../models/video";
+
+export const home = async (req, res) => {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
+};
 
 export const seeVideo = (req, res) => {
     const { id } = req.params;
@@ -7,20 +12,20 @@ export const seeVideo = (req, res) => {
 
 export const editVideo = (req, res) => {
     const { id } = req.params;
-    res.render("edit", { pageTitle: `Editing`});
-}
+    res.render("edit", { pageTitle: `Editing` });
+};
 
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
     return res.redirect(`/videos/${id}`);
-}
+};
 
 export const getUploadVideo = (req, res) => {
     res.render("upload", { pageTitle: "Upload Video" });
-}
+};
 
 export const postUploadVideo = (req, res) => {
     const { title } = req.body;
     return res.redirect("/");
-}
+};
